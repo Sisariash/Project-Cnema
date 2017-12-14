7149c5e3565a1ab55447534e7e92e1149eeb1789
Osman <osman.gueloglu@stud.fh-rosenheim.de>
2017-12-15 00:54:29 +0100

    Saal Test, Vorstellung Tests hinzzugefügt

--- Cnema/CnemaUnitTest/KinoprogrammverwaltungTest.cs
+++ Cnema/CnemaUnitTest/KinoprogrammverwaltungTest.cs
@@ -18,35 +18,27 @@ namespace CnemaUnitTest
         IKinoprogrammverwaltung kinoprogrammverwaltung = new Kinoprogrammverwaltung();
         IDatenbankManager datenbankManager = new DatenbankManager();
 
+        //FilmTests
 
         [TestMethod]
 
         public void FilmHinzufuegenTest()
         {
 
-           
-            Film film1  = new Film("The Dark Knight", 2008, "Action", 152, "Deutsch", false, 9.0, 16);
-            Film film2  = new Film("Star Wars 7", 2017, "Action", 287, "Deutsch", true, 8.8, 6);
-            Film film3  = new Film("Star Wars 7", 2017, "Action", 287, "Deutsch", false, 8.8, 6);
-            Film film4  = new Film("Cars3", 2017, "Animation", 110, "Deutsch", false, 7.6, 0);
-            Film film5  = new Film("Bad Moms 2", 2017, "Komödie", 105, "Deutsch", false, 7.0, 12);
-            Film film6  = new Film("Jigsaw", 2017, "Horror", 92, "Deutsch", false, 8.2, 18);
-            Film film7  = new Film("Der Pate", 1972, "Krimi", 175, "Deutsch", false, 9.2, 16);
-            Film film8  = new Film("Pulp Fiction", 1994, "Drama", 154, "Deutsch", false, 8.9, 16);
-            Film film9  = new Film("Yol Arkadasim", 2017, "Komödie", 115, "Türkisch", false, 7.0, 6);
+
+            Film film1 = new Film("The Dark Knight", 2008, "Action", 152, "Deutsch", false, 9.0, 16);
+            Film film2 = new Film("Star Wars 7", 2017, "Action", 287, "Deutsch", true, 8.8, 6);
+            Film film3 = new Film("Star Wars 7", 2017, "Action", 287, "Deutsch", false, 8.8, 6);
+            Film film4 = new Film("Cars3", 2017, "Animation", 110, "Deutsch", false, 7.6, 0);
+            Film film5 = new Film("Bad Moms 2", 2017, "Komödie", 105, "Deutsch", false, 7.0, 12);
+            Film film6 = new Film("Jigsaw", 2017, "Horror", 92, "Deutsch", false, 8.2, 18);
+            Film film7 = new Film("Der Pate", 1972, "Krimi", 175, "Deutsch", false, 9.2, 16);
+            Film film8 = new Film("Pulp Fiction", 1994, "Drama", 154, "Deutsch", false, 8.9, 16);
+            Film film9 = new Film("Yol Arkadasim", 2017, "Komödie", 115, "Türkisch", false, 7.0, 6);
             Film film10 = new Film("Paddington 2", 2017, "Annimation", 104, "Detusch", false, 6.0, 0);
 
-            film1.FilmId = 1;
-            film1.FilmId = 2;
-            film1.FilmId = 3;
-            film1.FilmId = 4;
-            film1.FilmId = 5;
-            film1.FilmId = 6;
-            film1.FilmId = 7;
-            film1.FilmId = 8;
-            film1.FilmId = 9;
-            film1.FilmId = 10;
-            
+           
+
 
 
             kinoprogrammverwaltung.FilmHinzufuegen(film1);
@@ -66,14 +58,14 @@ namespace CnemaUnitTest
 
 
 
-        }  
+        }
 
         [TestMethod]
 
         public void FilmLoeschenTest()
         {
 
-            
+
             Film film1 = kinoprogrammverwaltung.FilmLesen(1);
             kinoprogrammverwaltung.FilmLoeschen(film1);
 
@@ -104,7 +96,7 @@ namespace CnemaUnitTest
             Film film10 = kinoprogrammverwaltung.FilmLesen(10);
             kinoprogrammverwaltung.FilmLoeschen(film10);
 
-            
+
             Film filmFromDb1 = kinoprogrammverwaltung.FilmLesen(1);
             Assert.IsNull(filmFromDb1);
 
@@ -137,7 +129,7 @@ namespace CnemaUnitTest
 
 
         }
-        
+
         [TestMethod]
         public void FilmAendernTest()
         {
@@ -160,6 +152,129 @@ namespace CnemaUnitTest
             ///Assert.AreEqual(geanderterFilm.Jahr, film.Jahr);
         }
 
+        //SaalTests
+
+        [TestMethod]
+        public void SaalHinzufuegenTest()
+        {
+
+
+            Saal saal1 = new Saal("Saal1", 150);
+            Saal saal2 = new Saal("Saal2", 100);
+            Saal saal3 = new Saal("Saal3", 75);
+
+
+
+
+            datenbankManager.SaalHinzufügen(saal1);
+            datenbankManager.SaalHinzufügen(saal2);
+            datenbankManager.SaalHinzufügen(saal3);
+
+        }
+
+        [TestMethod]
+
+        public void SaalLoeschenTest()
+        {
+
+
+            Saal saal1 = datenbankManager.SaalLesen("saal1");
+            datenbankManager.SaalLoeschen(saal1);
+
+            Saal saal2 = datenbankManager.SaalLesen("saal2");
+            datenbankManager.SaalLoeschen(saal2);
+
+            Saal saal3 = datenbankManager.SaalLesen("saal3");
+            datenbankManager.SaalLoeschen(saal3);
+
+
+
+
+
+            Saal saalFromDb1 = datenbankManager.SaalLesen("saal1");
+            Assert.IsNull(saalFromDb1);
+
+            Saal saalFromDb2 = datenbankManager.SaalLesen("saal2");
+            Assert.IsNull(saalFromDb2);
+
+            Saal saalFromDb3 = datenbankManager.SaalLesen("saal3");
+            Assert.IsNull(saalFromDb3);
+
+        }
+        //VorstellungTests
+        [TestMethod]
+        public void VorstellungHinzufuegenTest()
+        {
+
+            // Klappt nicht immer wieso? Fügt manchmal hinzu beim laufen lassen manchmal nicht 
+            
+
+            Film film1 = new Film("The Dark Knight", 2008, "Action", 152, "Deutsch", false, 9.0, 16);
+            Film film2 = new Film("Star Wars 7", 2017, "Action", 287, "Deutsch", true, 8.8, 6);
+            Film film3 = new Film("Star Wars 7", 2017, "Action", 287, "Deutsch", false, 8.8, 6);
+
+            Saal saal1 = new Saal("Saal1", 150);
+            Saal saal2 = new Saal("Saal2", 100);
+            Saal saal3 = new Saal("Saal3", 75);
+
+            
+            Vorstellung vorstellung1 = new Vorstellung(film1, saal1, DateTime.Now);
+            Vorstellung vorstellung2 = new Vorstellung(film2, saal2, DateTime.Now);
+            Vorstellung vorstellung3 = new Vorstellung(film3, saal3, DateTime.Now);
+           
+
+            datenbankManager.VorstellungHinzufuegen(vorstellung1);
+            datenbankManager.VorstellungHinzufuegen(vorstellung2);
+            datenbankManager.VorstellungHinzufuegen(vorstellung3);
+        }
+    
+
+    [TestMethod]
+
+    public void VorstellungLoeschenTest()
+    {
+
+
+        Vorstellung vorstellung1 = datenbankManager.VorstellungLesen(10);
+        datenbankManager.VorstellungLoeschen(vorstellung1);
+
+        Vorstellung vorstellung2 = datenbankManager.VorstellungLesen(11);
+         datenbankManager.VorstellungLoeschen(vorstellung2);
+
+        Vorstellung vorstellung3 = datenbankManager.VorstellungLesen(12);
+        datenbankManager.VorstellungLoeschen(vorstellung3);
+
+
+
+
+
+        Vorstellung saalFromDb1 = datenbankManager.VorstellungLesen(10);
+        Assert.IsNull(saalFromDb1);
+
+        Vorstellung saalFromDb2 = datenbankManager.VorstellungLesen(11);
+        Assert.IsNull(saalFromDb2);
+
+        Vorstellung saalFromDb3 = datenbankManager.VorstellungLesen(12);
+        Assert.IsNull(saalFromDb3);
+
+    }
+
+        [TestMethod]
+        public void VorstellungAendernTest()
+        {
+
+            // Ist Grün aber passiert nichts???
+
+            Saal saal1 = new Saal("MegaSaal", 150);
+            
+
+            Vorstellung vorstellung1 = datenbankManager.VorstellungLesen(15);
+            vorstellung1.DateTime = DateTime.Now;
+            vorstellung1.Saal = saal1;
+            
+        
+    
+        }
     }
 }
 
---
+++ Cnema/dbo.Vorstellungs.data.sql
@@ -0,0 +1,5 @@
+﻿SET IDENTITY_INSERT [dbo].[Vorstellungs] ON
+INSERT INTO [dbo].[Vorstellungs] ([VorstellungId], [DateTime], [Film_FilmId], [Saal_SaalName]) VALUES (13, N'2017-12-15 00:31:59', 183, N'Saal1')
+INSERT INTO [dbo].[Vorstellungs] ([VorstellungId], [DateTime], [Film_FilmId], [Saal_SaalName]) VALUES (14, N'2017-12-15 00:31:59', 184, N'Saal2')
+INSERT INTO [dbo].[Vorstellungs] ([VorstellungId], [DateTime], [Film_FilmId], [Saal_SaalName]) VALUES (15, N'2017-12-15 00:31:59', 185, N'Saal3')
+SET IDENTITY_INSERT [dbo].[Vorstellungs] OFF
