45001234c82559c09647dbdcb5a1f731b42587fe
Osman <osman.gueloglu@stud.fh-rosenheim.de>
2017-12-18 21:03:44 +0100

    GUI erstellt (KundenRegistrierung, Kinoprogramm, BestaetigungDerRgistirerung)

---
+++ Cnema/GUI/Kinoprogramm.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUI
+{
+    /// <summary>
+    /// Interaktionslogik für Kinoprogramm.xaml
+    /// </summary>
+    public partial class Kinoprogramm : Page
+    {
+        public Kinoprogramm()
+        {
+            InitializeComponent();
+        }
+
+        private void Tag_ListView(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Film_ListView(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Uhrzeit_ListView(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Buchung_Button(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FilmeBewerten_Button(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FilmDetails_Button(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Cnema/GUI/KundenRegistrierung.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUI
+{
+    /// <summary>
+    /// Interaktionslogik für KundenRegistrierung.xaml
+    /// </summary>
+    public partial class KundenRegistrierung : Page
+    {
+        public KundenRegistrierung()
+        {
+            InitializeComponent();
+        }
+
+        private void Name_Textbox(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Vorname_TextBox(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Geburtstag_TextBox(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        // !!!ACHTUNG!!! Selber geschrieben die Methode ging nicht mit Doppelklick erzeugen
+        private void WaehlePasswort_PasswortBox(object sender, TextChangedEventArgs e)
+        {
+            
+
+        }
+
+        // !!!ACHTUNG!!! Selber geschrieben die Methode ging nicht mit Doppelklick erzeugen
+        private void WiederholePasswort_PasswortBox(object sender, TextChangedEventArgs e)
+        {
+            
+
+        }
+
+        private void Registrieren_Button(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
