43221061db9e02370fb1d599e898b54a79049278
Osman <osman.gueloglu@stud.fh-rosenheim.de>
2017-12-21 18:27:29 +0100

    Die restlichen GUI erstellt(Alle GUI Fertig!)

---
+++ Cnema/GUI/FilmBewerten.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUI
+{
+    /// <summary>
+    /// Interaktionslogik für FilmBewerten.xaml
+    /// </summary>
+    public partial class FilmBewerten : Page
+    {
+        public FilmBewerten()
+        {
+            InitializeComponent();
+        }
+
+        private void Ein_CheckBox(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Zwei_CheckBox(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Drei_CheckBox(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Vier_CheckBox(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Fuenf_CheckBox(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Bewertung_Button(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+
+ 
+    }
+
---
+++ Cnema/GUI/FilmeFiltern.xaml.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUI
+{
+    /// <summary>
+    /// Interaktionslogik für FilmeFiltern.xaml
+    /// </summary>
+    public partial class FilmeFiltern : Page
+    {
+        public FilmeFiltern()
+        {
+            InitializeComponent();
+        }
+
+        private void Montag_CheckBox(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Dienstag_CheckBox(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Mittwoch_CheckBox(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Donnerstag_CheckBox(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Freitag_CheckBox(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Samstag_CheckBox(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Sonntag_CheckBox(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Wochenende_CheckBox(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Uhr1700_CheckBox(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Uhr1930_CheckBox(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Uhr2200_CheckBox(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Uhr1100_CheckBox(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Uhr1300_CheckBox(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void zweiD_CheckBox(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void dreiD_CheckBox(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Action_CheckBox(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Fantasy_CheckBox(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Drama_CheckBox(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Abenteuer_CheckBox(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Horror_CheckBox(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Komoedie_CheckBox(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Krimi_CheckBox(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Liebe_CheckBox(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Animiert_CheckBox(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FilmName_TextBox(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Suchen_Button(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
