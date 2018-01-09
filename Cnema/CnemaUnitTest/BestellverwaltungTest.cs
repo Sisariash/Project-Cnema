1c1a04df64d5239146c399f690ec96090a9983d9
sWIjohamb <jonathan.hamberger@stud.fh-rosenheim.de>
2018-01-09 10:42:37 +0100

    Test für Reservierung überarbeitet.

--- Cnema/CnemaUnitTest/BestellverwaltungTest.cs
+++ Cnema/CnemaUnitTest/BestellverwaltungTest.cs
@@ -5,6 +5,8 @@ using Komponenten.ET;
 using Komponenten.Bestellverwaltung.Impl;
 using Komponenten.Kundenverwaltung.Impl;
 using Komponenten.Datenbank.Impl;
+using Komponenten.Util;
+using Komponenten.Kinoprogrammverwaltung.Impl;
 
 namespace CnemaUnitTest
 {
@@ -17,9 +19,21 @@ namespace CnemaUnitTest
             DatenbankManager dbm = DatenbankManager.Instance;
             Bestellverwaltung bestellverwaltung = new Bestellverwaltung(dbm);
             Kundenverwaltung kundenverwaltung = new Kundenverwaltung(dbm);
-            Kunde testKunde = dbm.KundeLesen(3);
-            Vorstellung testVorstellung = dbm.VorstellungLesen(1);
+            Kunde testKunde;
+            kundenverwaltung.KundeRegistrieren(Utils.HashPassword("1234"), "Wurst", "Hans", new DateTime(1990, 3, 15), out testKunde);
+            Film testFilm = new Film("Test", 2000, "Action", 110, "Deutsch", false, 18);
+            dbm.FilmHinzufuegen(testFilm);
+            Vorstellung testVorstellung = new Vorstellung(testFilm, dbm.SaalLesen("Saal 1"), DateTime.Now);
+            dbm.VorstellungHinzufuegen(testVorstellung);
+            //Bestellung erzeugen
             bestellverwaltung.Reservieren(testKunde, testVorstellung);
+            Assert.AreEqual(1, testKunde.Bestellungen.Count);
+            //Beim Löschen der Vorstellung soll auch die Bestellung gelöscht werden
+            dbm.VorstellungLoeschen(testVorstellung);
+            Assert.AreEqual(0, testKunde.Bestellungen.Count);
+            //Testdaten löschen
+            dbm.FilmLoeschen(testFilm);
+            dbm.KundeLoeschen(testKunde);
         }
     }
 }
